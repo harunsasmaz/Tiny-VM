62f1d208a81e50b1130fc975a1538d563b280a62
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-04 23:57:17 +0300

    memory access  functions added

--- tvm.cpp
+++ tvm.cpp
@@ -137,4 +137,38 @@ int read_image(const char* image_path)
     return 1;
 }
 
+/* Check Key */
+uint16_t check_key()
+{
+    fd_set readfds;
+    FD_ZERO(&readfds);
+    FD_SET(STDIN_FILENO, &readfds);
 
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 0;
+    return select(1, &readfds, NULL, NULL, &timeout) != 0;
+}
+
+/* Memory Access */
+void mem_write(uint16_t address, uint16_t val)
+{
+    memory[address] = val;
+}
+
+uint16_t mem_read(uint16_t address)
+{
+    if (address == MR_KBSR)
+    {
+        if (check_key())
+        {
+            memory[MR_KBSR] = (1 << 15);
+            memory[MR_KBDR] = getchar();
+        }
+        else
+        {
+            memory[MR_KBSR] = 0;
+        }
+    }
+    return memory[address];
+}
