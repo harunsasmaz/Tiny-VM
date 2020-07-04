5336afc31d37d86c41c539082f0069ed57629466
harunsasmaz <hsasmaz16@ku.edu.tr>
2020-07-04 23:52:26 +0300

    reading image file functions added

--- tvm.cpp
+++ tvm.cpp
@@ -107,3 +107,34 @@ void update_flags(uint16_t r)
         reg[R_COND] = FL_POS;
 }
 
+/* Read Image File */
+void read_image_file(FILE* file)
+{
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    // the maximum file size, one fread is enough
+    uint16_t max_read = UINT16_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    /* swap to little endian */
+    while (read-- > 0)
+    {
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+/* Read Image */
+int read_image(const char* image_path)
+{
+    FILE* file = fopen(image_path, "rb");
+    if (!file) { return 0; };
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
+
